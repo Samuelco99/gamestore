38c32023ae9d6ff6bd1099481e015908009491c6
unknown <samuelco.ti18@utsjr.edu.mx>
2021-11-21 22:21:16 -0600

    notifications-update

--- js/notification.js
+++ js/notification.js
@@ -1,9 +1,13 @@
-Push.create("Game-Store", {
-    body: "Nuevas noticias ve al apartado de noticias o haz click aqui",
-    icon: './icons/icon32.png',
-    timeout: 4000,
-    onclick: function(){
-        window.location="./pages/noticias.html";
-        this.close();
-    }
-});
+//await Notification.requestPermission();
+
+async function showNotification() {
+	const result = await Notification.requestPermission();
+	if (result === 'granted') {
+		const noti = new Notification('Noticias Nuevas', {
+			body: 'Nuevas noticias ve al apartado de noticias o haz click aqui',
+			icon: './icons/icon32.png'
+		});
+		noti.onclick = () => window.location="./pages/noticias.html";
+	}
+}
+showNotification();
